138930a66a575785c2baec12eeea801d48a9a8ec
Jan Randa <jnranda@gmail.com>
2023-05-12 10:01:23 +0200

    add todo

---
+++ class/renderPage.php
@@ -0,0 +1,21 @@
+<?php
+
+
+function renderPage($body, $title) {
+return '
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Vyšetřovatel | '.$title.'</title>
+        <link rel="stylesheet" href="./style.css">
+        <link rel="icon" href="./favicon.ico" type="image/x-icon">
+    </head>
+    <body>
+        '.$body.'
+    </body>
+</html>
+';
+}
---
+++ help/todo.md
@@ -0,0 +1,9 @@
+
+TODO:
+
+Jaké budou ve hře scény (cca 3)
+Jaký bude příběh
+Jaké budou další postavy
+Jaké budou předměty 
+Jak bude hra začínat
+Jak bude končit
